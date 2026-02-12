e647c51a156f680fc4aa682d9cdf74df2c248100
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-12 18:47:58 +0530

    Time: 3 ms (56.76%) | Memory: 46.6 MB (37.46%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
         int n=words.length;
-        List<Map<Character,Integer>> freq=new ArrayList<>();
-        for(String s:words){
-            Map<Character,Integer> map=new HashMap<>();
-            for(char c:s.toCharArray()){
-                map.put(c,map.getOrDefault(c,0)+1);
-            }
-            freq.add(map);
-        }
         List<String> ans=new ArrayList<>();
-        ans.add(words[0]);
-        for(int i=1;i<n;i++){
-            if(!freq.get(i).equals(freq.get(i-1))){
-                ans.add(words[i]);
+        String prev="";
+        for(String w:words){
+            char[] a=w.toCharArray();
+            Arrays.sort(a);
+            String sor=new String(a);
+            if(!sor.equals(prev)){
+                ans.add(w);
+                prev=sor;
             }
         }
         return ans;
