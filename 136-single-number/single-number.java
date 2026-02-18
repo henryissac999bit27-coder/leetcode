5117c29505dd4ed4752be87035be8d0580aa54f9
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-18 21:54:03 +0530

    Time: 14 ms (14.76%) | Memory: 46.6 MB (96.17%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int arr=-1;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                arr=entry.getKey();
+                break;
+            }
+        }
+        return arr;
+    }
+}
