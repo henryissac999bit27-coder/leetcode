9b1e56cbda5162432d2dfcc0f4eeb670cf147f6f
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-21 20:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (14.63%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans=new ArrayList<>();
+        int curr=1;
+        for(int i:target){
+            while(curr<i){
+                ans.add("Push");
+                ans.add("Pop");
+                curr++;
+            }
+            ans.add("Push");
+            curr++;
+        }
+        return ans;
+    }
+}
