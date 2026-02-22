739fed1dfb114daf6f34ade1ff03a74fff382d84
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-22 20:06:05 +0530

    Time: 23 ms (92.50%) | Memory: 103.6 MB (87.87%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] answer=new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > 
+            temperatures[stack.peek()]){
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex; 
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
