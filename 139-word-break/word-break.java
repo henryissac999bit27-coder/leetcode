172844e164bd49b1d4519b0d797f3c95e6f854d2
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-03-23 19:27:53 +0530

    Time: 8 ms (47.43%) | Memory: 46.5 MB (8.27%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -19,18 +19,19 @@
 }*/
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length() + 1];
+        HashSet<String> dict = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
         dp[0] = true;
 
-        for (int i = 1; i <= s.length(); i++) {
-            for (String w : wordDict) {
-                int start = i - w.length();
-                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
             }
         }
-        return dp[s.length()];        
+        return dp[n];
     }
 }
