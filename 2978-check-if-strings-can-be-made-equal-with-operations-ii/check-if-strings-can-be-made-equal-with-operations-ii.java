5e37809c6fdd970e50fa27b07fcd17799942384a
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-03-30 20:20:24 +0530

    Time: 6 ms (61.84%) | Memory: 47.8 MB (85.53%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean checkStrings(String s1, String s2) {
+    /*public boolean checkStrings(String s1, String s2) {
         StringBuilder ee1=new StringBuilder();
         StringBuilder ee2=new StringBuilder();
         StringBuilder oo1=new StringBuilder();
@@ -24,5 +24,18 @@ class Solution {
         Arrays.sort(o1);
         Arrays.sort(o2);
         return Arrays.equals(e1,e2) && Arrays.equals(o1,o2);
+    }*/
+    public boolean checkStrings(String s1, String s2){
+        int[][] freq=new int[2][26];
+        for(int i=0;i<s1.length();i++){
+            freq[i%2][s1.charAt(i)-'a']++;
+            freq[i%2][s2.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[0][i]!=0||freq[1][i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }
