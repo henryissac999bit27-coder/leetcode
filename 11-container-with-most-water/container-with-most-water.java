d126fd21b8e91b64e29f6949fff6a73e0beaa1cf
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-03-31 22:07:30 +0530

    Time: 5 ms (82.88%) | Memory: 77.3 MB (60.08%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea = 0;
-        int left = 0;
-        int right = height.length - 1;
-
-        while (left < right) {
-            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
-
-            if (height[left] < height[right]) {
+        int max=0;
+        int left=0;
+        int right=height.length-1;
+        while(left<right){
+            max=Math.max(max,(right-left)*Math.min(height[left],height[right]));
+            if(height[left]<height[right]){
                 left++;
-            } else {
+            }
+            else{
                 right--;
             }
         }
-
-        return maxArea;        
+        return max;
     }
 }
