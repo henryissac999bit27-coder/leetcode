f8c99d8bef8a75e2cddc5ab8b6f5fa73e9481b2d
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-04-09 22:08:03 +0530

    Time: 1462 ms (5.03%) | Memory: 52.6 MB (82.46%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,7 +10,7 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    /*public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode ptr1=headA;
         ListNode ptr2=headB;
         while(ptr1!=ptr2){
@@ -18,5 +18,18 @@ public class Solution {
             ptr2=(ptr2!=null) ? ptr2.next:headA;
         }
         return ptr1;
+    }*/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while(headB!=null){
+            ListNode temp=headA;
+            while(temp!=null){
+                if(temp==headB){
+                    return headB;
+                }
+                temp=temp.next;
+            }
+            headB=headB.next;
+        }
+        return null;
     }
 }
