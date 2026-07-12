518851be07fcd5522bf7e1ba5df9f00b330ffc65
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-07-12 21:04:33 +0530

    Time: 2 ms (31.40%) | Memory: 47.5 MB (21.67%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-        int jump=0,curr=0,max=0;
+        int curr=0,jump=0,max=0;
         for(int i=0;i<nums.length-1;i++){
             max=Math.max(max,i+nums[i]);
             if(curr==i){
                 jump++;
                 curr=max;
             }
-            if(curr>nums.length-1) break;
+            if(curr>=nums.length-1) break;
         }
         return jump;
     }
