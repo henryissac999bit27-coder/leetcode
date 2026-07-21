93eb5dbbae7af78cebd808fc8228614c92c3604d
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-07-21 20:53:07 +0530

    Time: 12 ms (51.56%) | Memory: 46.8 MB (72.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res=new ArrayList<>();
+        /*List<Integer> res=new ArrayList<>();
         char[] parr=p.toCharArray();
         Arrays.sort(parr);
         String p1=new String(parr);
@@ -12,6 +12,22 @@ class Solution {
             Arrays.sort(arr);
             String s1=new String(arr);
             if(p1.equals(s1)) res.add(i);            
+        }
+        return res;*/
+        List<Integer> res=new ArrayList<>();
+        int[] pcount=new int[26];
+        int[] scount=new int[26];
+        for(int i=0;i<p.length();i++){
+            pcount[p.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            scount[s.charAt(i)-'a']++;
+            if(i>=p.length()){
+                scount[s.charAt(i-p.length())-'a']--;
+            }
+            if(Arrays.equals(scount,pcount)){
+                res.add(i-p.length()+1);
+            }
         }
         return res;
     }
