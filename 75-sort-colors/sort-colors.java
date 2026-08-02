9c9dda8f137d0fa77ee2e921da21f71dff0b249e
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-08-02 20:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (97.81%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,21 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        count.put(0, 0);
-        count.put(1, 0);
-        count.put(2, 0);
-
-        for (int num : nums) {
-            count.put(num, count.get(num) + 1);
-        }
-
-        int idx = 0;
-        for (int color = 0; color < 3; color++) {
-            int freq = count.get(color);
-            for (int j = 0; j < freq; j++) {
-                nums[idx] = color;
-                idx++;
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high){
+            if (nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
             }
-        }        
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+        }
     }
 }
