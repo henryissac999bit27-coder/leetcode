528f5c9b0f7ab8a47fadf79d4767356948486e1f
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-08-06 15:48:32 +0530

    Time: 1827 ms (15.61%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,3 +1,7 @@
-SELECT name from SalesPerson where sales_id NOT IN
-(select sales_id from Orders o join Company c on o.com_id=c.com_id
+SELECT name 
+from SalesPerson  as s 
+where s.sales_id NOT IN
+(select o.sales_id 
+from Orders as o join Company as c 
+on o.com_id=c.com_id
 where c.name ="RED");
