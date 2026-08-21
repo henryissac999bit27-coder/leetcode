06a74df906b4f3d63acac78cee7002aafbf78dc3
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-08-21 21:55:44 +0530

    Time: 2 ms (91.97%) | Memory: 47.8 MB (38.60%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,29 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] != 0) {
-                int temp = nums[right];
-                nums[right] = nums[left];
-                nums[left] = temp;
+        int left=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]!=0){
+                int temp=nums[right];
+                nums[right]=nums[left];
+                nums[left]=temp;
                 left++;
             }
-        }        
+        }
     }
 }
-/*
-class Solution {
-    public void moveZeroes(int[] nums) {
-         int index = 0; 
-        for (int num : nums) { 
-            if (num != 0) { 
-                nums[index++] = num; 
-            } 
-        } 
-        while (index < nums.length) { 
-            nums[index++] = 0; 
-        } 
-    }
-}
-*/
